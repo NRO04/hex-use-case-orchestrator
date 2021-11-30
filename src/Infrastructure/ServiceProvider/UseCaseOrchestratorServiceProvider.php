23622070087fb327514388a7b44116adf5aa7281
nro04 <ronicolas04@gmail.com>
2021-11-30 02:36:36 -0500

    Fix Path use-case-handlers

--- src/Infrastructure/ServiceProvider/UseCaseOrchestratorServiceProvider.php
+++ src/Infrastructure/ServiceProvider/UseCaseOrchestratorServiceProvider.php
@@ -3,12 +3,16 @@
 namespace Ro\HexUseCaseOrchestrator\Infrastructure\ServiceProvider;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\Application as LaravelApplication;
 use Ro\HexUseCaseOrchestrator\Infrastructure\Orchestrator\UseCaseOrchestrator;
 
 class UseCaseOrchestratorServiceProvider extends ServiceProvider
 {
     public function register()
     {
+        $this->mergeConfigFrom(
+            $this->configPath(), 'use-case-handlers'
+        );
         $this->app->singleton(UseCaseOrchestrator::class, function ($app) {
             return new UseCaseOrchestrator($this->loadHandlers());
         });
@@ -16,13 +20,21 @@ class UseCaseOrchestratorServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        $this->publishes([
-            __DIR__ . '/../config/use-case-handlers.php' => config_path('use-case-handlers.php'),
-        ], 'use-case-handlers');
+        if ($this->app instanceof LaravelApplication && $this->app->runningInConsole()) {
+
+            $this->publishes([
+                $this->configPath() => config_path('use-case-handlers.php'),
+            ], 'use-case-handlers');
+        }
     }
 
     public function loadHandlers()
     {
         return $this->app['config']->get('use-case-handlers');
+    }
+
+    function configPath(): string
+    {
+        return __DIR__ . '/../../../config/use-case-handlers.php';
     }
 }
