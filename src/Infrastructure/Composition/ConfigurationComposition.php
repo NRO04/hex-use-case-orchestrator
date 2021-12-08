39da3a8ebbeec4a097ab60c4089080ba025d9ca9
nro04 <ronicolas04@gmail.com>
2021-12-07 22:12:00 -0500

    Documentation added in configuration composition

--- src/Infrastructure/Composition/ConfigurationComposition.php
+++ src/Infrastructure/Composition/ConfigurationComposition.php
@@ -5,24 +5,52 @@ namespace Ro\HexUseCaseOrchestrator\Infrastructure\Composition;
 use Exception;
 use Ro\HexUseCaseOrchestrator\Domain\Repository\CompositionApiRepository;
 
+/**
+ * Class ConfigurationComposition
+ * Validates the configuration composition from the configuration file
+ */
 class ConfigurationComposition implements CompositionApiRepository
 {
+    /**
+     * Contains the base configuration composition
+     */
     private array $baseConfigurationApi = [
         'handler-composition-api' => 'required',
         'handlers' => 'required',
         'logs' => 'required'
     ];
 
+    /**
+     * Contains the configuration composition that has the configuration file.
+     */
     private array $configurationApiComposition = [];
 
     function __construct(array $configuration_api_composition)
     {
         $this->configurationApiComposition = $configuration_api_composition;
     }
-
-    public function getConfigurationCompositionApi(): array
+    /**
+     * Gets the configuration composition from the configuration file
+     */
+    function getConfigurationCompositionApi(): array
     {
         return $this->configurationApiComposition;
+    }
+
+    function getConfigKeys(): array
+    {
+        return array_keys($this->baseConfigurationApi);
+    }
+
+    /**
+     * @throws Exception
+     */
+    function getConfigOption(string $key): array
+    {
+        if (!array_key_exists($key, $this->configurationApiComposition)) {
+            throw new Exception("Key {$key} not found in configuration");
+        }
+        return $this->configurationApiComposition[$key];
     }
 
     /**
@@ -32,7 +60,7 @@ class ConfigurationComposition implements CompositionApiRepository
     {
         foreach ($this->getConfigurationCompositionApi() as $config_option => $value_option) {
             if (!array_key_exists($config_option, $this->baseConfigurationApi)) {
-                throw new Exception("The option: '$config_option' is not valid");
+                throw new Exception("The option: '$config_option' is not a valid option, please check the config file");
             }
         }
     }
--- src/Infrastructure/build/BuildHandlers.php
+++ src/Infrastructure/build/BuildHandlers.php
@@ -4,17 +4,29 @@ namespace Ro\HexUseCaseOrchestrator\Infrastructure\build;
 
 use ReflectionException;
 use Ro\HexUseCaseOrchestrator\Domain\Repository\CompositionApiRepository;
+use Ro\HexUseCaseOrchestrator\Infrastructure\Composition\ConfigurationComposition;
 use Ro\HexUseCaseOrchestrator\Infrastructure\Composition\ValidateComposition;
 
 class BuildHandlers implements CompositionApiRepository
 {
     private ValidateComposition $validateComposition;
     private array $handlers;
+    private ConfigurationComposition $configuration;
 
     function __construct(array $configuration)
     {
         $this->validateComposition = new ValidateComposition($configuration);
-        $this->handlers = $this->validateComposition->getHandlers();
+        $this->configuration = new ConfigurationComposition($configuration);
+        $this->execute();
+    }
+
+    /**
+     * @throws \Exception
+     */
+    function boot()
+    {
+        $this->handlers = $this->configuration->getConfigOption('handlers');
+
     }
 
     function execute(): void
@@ -29,7 +41,15 @@ class BuildHandlers implements CompositionApiRepository
     function compose(): array
     {
         $handlers_composited = array();
+        $dependency = '';
+        $handler_class = '';
         foreach ($this->handlers as $handler_name => $handler) {
+
+            foreach () {
+
+            }
+
+
             $handlers_composited["$handler_name"] = $this->bind(
 
             //Creates a new instance of the dependency which defined in the config file. By default, this dependency it is call service
@@ -63,11 +83,12 @@ class BuildHandlers implements CompositionApiRepository
         }
     }
 
+
     /**
      * Binds a handler with a dependency, and returns a new instance of the handler with the dependency injected.
      * @throws ReflectionException
      */
-    function bind(object $dependency, $handler): object
+    function bind(object $dependency, string $handler): object
     {
         $instance = new \ReflectionClass($handler);
         return $instance->newInstance($dependency);
