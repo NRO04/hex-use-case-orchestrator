0982074a732ae1fa0e79157d7c26ecb9ae0faddb
nro04 <ronicolas04@gmail.com>
2021-12-08 16:50:44 -0500

    Check if there are handlers configured

--- src/Infrastructure/Composition/HandlersComposition.php
+++ src/Infrastructure/Composition/HandlersComposition.php
@@ -13,6 +13,7 @@ class HandlersComposition implements CompositionApiRepository
     ];
     private array $handlerCompositionApi;
     private array $reverseHandlersComposition; // reverse of handler composition api
+    private array $handlers;
 
     /**
      * @throws Exception
@@ -20,6 +21,8 @@ class HandlersComposition implements CompositionApiRepository
     function __construct(array $configuration)
     {
         $this->handlerCompositionApi = $configuration['handler-composition-api'];
+        $this->handlers = $configuration['handlers'];
+        $this->checkIfHandlersAreSet();
         $this->reverseStructureOfTheComposition();
         $this->execute();
     }
@@ -37,6 +40,16 @@ class HandlersComposition implements CompositionApiRepository
     function checkIfAliasExists(string $alias_name): bool
     {
         return array_key_exists($alias_name, $this->reverseHandlersComposition);
+    }
+
+    /**
+     * @throws Exception
+     */
+    function checkIfHandlersAreSet(): void
+    {
+        if (!isset($this->handlers)) {
+            throw new Exception("There are not Handlers set, check handlers section in the configuration file use-case-orchestrator.php");
+        }
     }
 
     /**
