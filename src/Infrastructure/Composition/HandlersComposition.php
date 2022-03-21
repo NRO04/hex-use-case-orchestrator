9bf9b3f293a5c3f98887434d7722ae565d63cb98
Nicolas RO <ronicolas04@gmail.com>
2022-03-21 02:03:54 -0500

    feat(handler): add new method to validate handler composition api
    add recursive call to enter into sub compostions to validate against the base composition

--- src/Infrastructure/Composition/HandlersComposition.php
+++ src/Infrastructure/Composition/HandlersComposition.php
@@ -63,11 +63,12 @@ class HandlersComposition implements CompositionApiRepository
      */
     function execute(): void
     {
-        foreach ($this->getHandlerCompositionApi() as $key => $handlerComposition) {
-            if (!array_key_exists("$key", $this->baseHandlerComposition)) {
-                throw new Exception("$key is not a valid option for handler-composition-api, please check the configuration file use-case-orchestrator.php");
-            }
-        }
+
+        $base_composition = $this->getHandlerCompositionFrom('base-composition');
+
+        $file_composition = $this->getHandlerCompositionFrom('configuration-file');
+
+        $this->validateComposition($base_composition, $file_composition);
     }
 
     function reverseStructureOfTheComposition(): void
@@ -102,4 +103,69 @@ class HandlersComposition implements CompositionApiRepository
 
         return $compositions[$source];
     }
+
+    function isSequentialArray(array $data): bool
+    {
+
+        return array_values($data) === $data; // Uses the comparison operator to validate if both are of the same type in this case both must be a sequential array.
+    }
+
+
+    /**
+     * @throws Exception
+     */
+    function validateKeyInComposition(string $key, array $composition): void
+    {
+
+        if (!array_key_exists($key, $composition))  // Validates if the current key exists in the array to compare
+
+            throw new Exception("Required key: ($key) not found in the validation process, please check that your api structure is correct");
+
+    }
+
+    /**
+     * @throws Exception
+     */
+    function validateComposition(array $base_composition, array $compare): void
+    {
+
+        while (!empty($base_composition)) { //Loop until the array is empty
+
+            // --------------------------------STEP 1---------------------------------------------------
+
+            $key_base_composition = array_key_last($base_composition); // Gets the last key of the array
+
+            $pop_item_base_composition = $base_composition[$key_base_composition]; // Gets the last item in the array
+
+            array_pop($base_composition); // Deletes the last item of the array
+
+
+            $this->validateKeyInComposition($key_base_composition, $compare); // Validates if the current key exists in the array to compare
+
+
+            // ---------------------------------STEP 2--------------------------------------------------
+
+            $key_compare = $key_base_composition; // Gets the current key in compare array
+
+            $pop_item_compare = $compare[$key_compare]; // Gets the last item in compare array
+
+
+            // ---------------------------------STEP 3--------------------------------------------------
+
+
+            if (!$this->isSequentialArray($pop_item_base_composition))  //validates if is assoc array
+
+                $this->validateComposition($pop_item_base_composition, $pop_item_compare); // Calling itself again but now with its internal values, means that it will send values that are assoc array to validate again and again until there are no more assoc array elements.
+
+            else
+
+                foreach ($pop_item_base_composition as $composition_value) { // Loop when the array is sequential, this will iterate each position validating that the key exists in the compare array
+
+                    $this->validateKeyInComposition($composition_value, $pop_item_compare); // Validates whether the current value of the sequential array exists as a key in the array to be compared.
+
+                }
+
+        }
+    }
+
 }
