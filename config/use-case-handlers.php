8a51d00c655821e43378fd9632b84a38f33a3369
nro04 <ronicolas04@gmail.com>
2022-03-22 16:49:12 -0500

    docs(configuration): add documentation about how to define aliases in handler-composition-api

--- config/use-case-handlers.php
+++ config/use-case-handlers.php
@@ -8,7 +8,7 @@ return [
   |   Put here the configuration for the use case handlers.
   |   You can add as many use case handlers as you want in handlers section.
   |   If you want to create a new use case handler, you must create a new class that implements UseCaseHandlerSchema
-       Remember to add the new class to the handlers' section for the use case handler to be used.
+      Remember to add the new class to the handlers' section for the use case handler to be used.
 
   */
 
@@ -34,17 +34,21 @@ return [
     ],
     //Defines how will be the composition of the use case handlers to be used it.
     'handler-composition-api' => [
-        /* handler property is reserved and this one, defines how the property will be nominated
-        in the handler section when the use case been statement.
-        You can change the default alias of the handler => @handler, if you want to use a different name.
-        It is important to know that the handler property value must be defined in the handler section.
-         And this one must be the same as the key of the handler in the handlers section.
+
+
+        /*
+        |--------------------------------------------------------------------------
+        | (Resolve)
+        |--------------------------------------------------------------------------
+        | You can change the default alias if you want to use a different name.
+        | It is important to know that the alias that was defined in the metadata, must also be defined in the handler section,
+        | And its value must be declared as a key in the handler section.
         */
         'resolve' => [
 
             'alias' => [
                 'handler' => '@handler',
-                'dependencies' => '@services',  //dependency defines how will be call it when the use case will be defined in the service section.
+                'dependencies' => '@services',
                 'use-cases' => '@useCases',
                 'compose' => '@compose'
             ]
